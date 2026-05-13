a9fd937cfa8602a316ebef64c38b4a38e1bfc09e
ritanshu patel <ritanshupatel.mmr@gmail.com>
2026-05-13 08:51:15 +0530

    fix: clean schema

--- backend/full_backup.sql
+++ backend/full_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict IHmbhqPUH7e56vhc91fNJFtfsn2djPfGlRfZimIimO5PET2w9zntaxXXrr3DfTc
+\restrict yxuw1cxKmho3Bg2ErArJId4smsjTbTA40UXRyO05IFItkqEZsJQGtV3FuUoh4Tf
 
 -- Dumped from database version 16.13 (Homebrew)
 -- Dumped by pg_dump version 16.13 (Homebrew)
@@ -275,6 +275,109 @@ ALTER TABLE ONLY public.tasks ALTER COLUMN id SET DEFAULT nextval('public.tasks_
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: project_members; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.project_members (project_id, user_id) FROM stdin;
+3	4
+4	5
+3	3
+2	2
+4	4
+5	6
+2	1
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.projects (id, name, description, start_date, end_date, status, created_by, created_at, updated_at) FROM stdin;
+2	Project Alpha	Main project	2026-01-01	2026-12-31	Active	1	2026-05-11 22:08:43.098+05:30	2026-05-11 22:08:43.098+05:30
+3	new project	testing	\N	2026-05-13	On Hold	1	2026-05-12 11:35:24.673+05:30	2026-05-12 11:35:24.673+05:30
+4	erp system	employee management system	\N	2026-07-31	Planned	1	2026-05-12 13:31:24.808+05:30	2026-05-12 13:31:24.808+05:30
+5	project one	build says app	\N	2026-05-31	Planned	1	2026-05-12 19:12:15.248+05:30	2026-05-12 19:12:15.248+05:30
+\.
+
+
+--
+-- Data for Name: task_comments; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.task_comments (id, task_id, user_id, comment, created_at) FROM stdin;
+1	1	3	Working on this now	2026-05-12 07:40:22.487+05:30
+2	3	1	kkkkk	2026-05-12 11:20:56.931+05:30
+3	3	1	complete	2026-05-12 11:36:03.932+05:30
+4	4	1	in process comment	2026-05-12 11:38:53.577+05:30
+5	5	1	work fast	2026-05-12 13:32:13.649+05:30
+6	6	1	hello	2026-05-12 17:58:51.158+05:30
+7	7	5	hai	2026-05-12 19:34:02.055+05:30
+8	8	6	hey	2026-05-12 19:53:52.907+05:30
+9	7	6	process	2026-05-12 19:54:21.922+05:30
+\.
+
+
+--
+-- Data for Name: tasks; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.tasks (id, title, description, project_id, assigned_to, created_by, status, priority, due_date, created_at, updated_at) FROM stdin;
+2	Fix login bug	\N	2	1	3	Todo	High	2026-06-01	2026-05-12 06:56:05.023+05:30	2026-05-12 06:56:05.023+05:30
+1	Fix login bug	\N	2	1	3	In Progress	Low	2026-06-01	2026-05-12 06:53:09.999+05:30	2026-05-12 07:10:32.497+05:30
+3	Dev task	\N	2	2	1	Completed	Low	\N	2026-05-12 07:33:34.632+05:30	2026-05-12 11:35:55.909+05:30
+4	few	fwrefrw	3	3	1	In Progress	Medium	\N	2026-05-12 11:38:36.764+05:30	2026-05-12 11:38:52.375+05:30
+5	database design	design good database	4	4	1	Todo	High	2026-05-20	2026-05-12 13:32:01.278+05:30	2026-05-12 13:32:01.278+05:30
+6	build backend	backend	4	5	1	Completed	Low	2026-05-30	2026-05-12 17:26:56.934+05:30	2026-05-12 17:58:45.021+05:30
+7	build front end	front end	5	6	1	In Progress	Medium	2026-05-31	2026-05-12 19:13:23.24+05:30	2026-05-12 19:14:08.015+05:30
+8	build database	creating new database	5	6	5	In Progress	Medium	2026-05-30	2026-05-12 19:34:59.824+05:30	2026-05-12 19:54:04.16+05:30
+9	make design for project	design	5	6	5	Todo	High	2026-05-14	2026-05-13 07:09:08.58+05:30	2026-05-13 07:09:08.58+05:30
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, email, password, role, is_active, created_at, updated_at) FROM stdin;
+1	Admin User	admin@test.com	$2b$10$BY1cZ65VD9sNMD4Knhie9O.p/IixkQBVyW32j1aJ3VZUgDdoO2gpC	Admin	t	2026-05-11 17:52:35.536+05:30	2026-05-11 17:52:35.536+05:30
+3	Manager User	manager@test.com	$2b$10$/KdVmzHaMoWagP1fTbSr0.go4REdzpGOUeGzrNP7zX0MSedeyFbNi	Manager	t	2026-05-12 06:45:55.194+05:30	2026-05-12 06:45:55.194+05:30
+2	Dev User	dev@test.com	$2b$10$/gl1IJ/4FzmWyoDDm9izOuy4AqVLl4MqBRqqrqBMBW74yD11JIcrW	Developer	t	2026-05-11 21:45:01.045+05:30	2026-05-12 07:21:03.295+05:30
+4	ritanshu 	ritanshupatel@gmail.com	$2b$10$HzEeGXcZD6YVATKZ2ZM3su0jWNWpT/MIwP.ldijVdP38jcuAdvJ7O	Developer	t	2026-05-12 11:21:54.838+05:30	2026-05-12 11:22:11.931+05:30
+5	Karan 	karan@gmail.com	$2b$10$JXEbfkBbM820Dck2aPEJh.B4fxVGs4zxON3OVrBGf5JpspcLpl1b2	Manager	t	2026-05-12 13:30:26.928+05:30	2026-05-12 18:00:23.634+05:30
+6	John 	john@gmail.com	$2b$10$SL5O/FKOLI2fbmXkxdWNPuwrUQAEkNOBatvtB.l9NMBcPr.jzNG1O	Developer	t	2026-05-12 19:12:49.888+05:30	2026-05-12 19:12:49.888+05:30
+\.
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+
+
+--
+-- Name: task_comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.task_comments_id_seq', 9, true);
+
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.tasks_id_seq', 9, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 6, true);
 
 
 --
@@ -705,5 +808,5 @@ ALTER TABLE ONLY public.tasks
 -- PostgreSQL database dump complete
 --
 
-\unrestrict IHmbhqPUH7e56vhc91fNJFtfsn2djPfGlRfZimIimO5PET2w9zntaxXXrr3DfTc
+\unrestrict yxuw1cxKmho3Bg2ErArJId4smsjTbTA40UXRyO05IFItkqEZsJQGtV3FuUoh4Tf
 
